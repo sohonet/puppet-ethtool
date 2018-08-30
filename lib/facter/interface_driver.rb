3f9515a50133873b4b1f0be29a5c99c11c014f28
Jason Pereira <mindriot88@users.noreply.github.com>
2018-08-30 18:28:31 +0100

    adjust command

--- lib/facter/interface_driver.rb
+++ lib/facter/interface_driver.rb
@@ -1,6 +1,6 @@
 require 'facter'
 require 'json'
-Facter::Util::Resolution.exec("ifconfig -l").split(" ").each do |interface|
+Facter::Util::Resolution.exec("ls /sys/class/net/").split(/\n/).each do |interface|
   next if interface.start_with?('veth') || interface.include?('lo') || ! File.exists?('/sbin/ethtool')
   Facter.debug("Running ethtool on interface #{interface}")
   data = {}
