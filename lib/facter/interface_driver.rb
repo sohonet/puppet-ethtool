32af44d713e8a9ab4ef068f2075ca4121c390000
Jason Pereira <mindriot88@users.noreply.github.com>
2018-08-31 10:42:23 +0100

    testing new method of fact gathering

--- lib/facter/interface_driver.rb
+++ lib/facter/interface_driver.rb
@@ -1,29 +1,39 @@
 require 'facter'
 require 'json'
-Facter::Util::Resolution.exec("basename -a $(ls /sys/devices/**/net/* -d)").split(/\n/).each do |interface|
-  next if interface.start_with?('veth') || interface.include?('lo') || ! File.exists?('/sbin/ethtool')
-  Facter.debug("Running ethtool on interface #{interface}")
-  data = {}
-  Facter::Util::Resolution.exec("ethtool -i #{interface} 2>/dev/null").split(/\n/).each do |line|
-    k, v = line.split(': ')
-    if v
-     data[k]=v
-    end
-  end
-  if data['driver']
-    Facter.add('driver_' + interface) do
-      confine :kernel => "Linux"
-      setcode do
-        data['driver']
+
+def get_interfaces
+  path = '/sys/class/net'
+  Dir.foreach(path).reject{|x| x.start_with?('.', 'veth', 'lo')}
+rescue Errno::ENOENT, Errno::EACCES => detail
+  Facter.debug "Could not read #{path}: #{detail.message}"
+end
+def set_facts
+  get_interfaces.each do |interface|
+    next if ! File.exists?('/sbin/ethtool')
+    Facter.debug("Running ethtool on interface #{interface}")
+    data = {}
+    Facter::Util::Resolution.exec("ethtool -i #{interface} 2>/dev/null").split(/\n/).each do |line|
+      k, v = line.split(': ')
+      if v
+       data[k]=v
       end
     end
-  end
-  if data['version']
-    Facter.add('driver_version_' + interface) do
-      confine :kernel => "Linux"
-      setcode do
-        data['version']
+    if data['driver']
+      Facter.add('driver_' + interface) do
+        confine :kernel => "Linux"
+        setcode do
+          data['driver']
+        end
+      end
+    end
+    if data['version']
+      Facter.add('driver_version_' + interface) do
+        confine :kernel => "Linux"
+        setcode do
+          data['version']
+        end
       end
     end
   end
 end
+set_facts
